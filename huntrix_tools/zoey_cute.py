0036f7003d4b841aecd85f7c6db55de887ddd62b
abinavvineria-tech <abinavvineria-tech@gmail.com>
2025-12-24 13:26:13 +0530

    new tool v9

---
+++ huntrix_tools/zoey_cute.py
@@ -0,0 +1,18 @@
+# lets check is zoey cute or not and young
+zoey_cuteness = int(input("On a scale of 1-1000, how cute is Zoey? "))
+if zoey_cuteness >= 800:
+    print(
+        "Zoey is very cute! and you love zoey <3 and you want to be their best friend"
+    )
+else:
+    print("Zoey is not that cute :(")
+zoey_age = int(input("How old is Zoey? "))
+if zoey_age > 18:
+    print("Zoey is cutest and youngest!")
+else:
+    exit()
+zoeys_friends = input("in kpop demon hunters who are shes friends")
+if zoeys_friends == "rumi":
+    print("Zoey is friends with Rumi, they are both cute!")
+else:
+    print("Zoey has other friends too!")
