a6afb1a9a4c9c6bf8e0681c6d36d668cd58ea9eb
abinavvineria-tech <abinavvineria-tech@gmail.com>
2025-12-24 11:16:14 +0530

    new tool for huntrix lovers

---
+++ coding_files/python/ai_tester-copliot/super_coder.py
@@ -0,0 +1,32 @@
+a = input("Do you love huntrix? (yes/no): ")
+b = input("Do you want to work with huntrix? (yes/no): ")
+c = input("Are you excited about huntrix? (yes/no): ")
+d = input("Do you think huntrix is awesome? (yes/no): ")
+e = input("Would you recommend huntrix to others? (yes/no): ")
+f = input("Do you believe huntrix can change the world? and the future (yes/no): ")
+g = input("Are you proud to be part of huntrix? (yes/no): ")
+if a == "yes" and b == "yes" and c == "yes" and d == "yes" and e == "yes" and f == "yes" and g == "yes":
+    print("You are a true huntrix super coder!")
+else:
+    print("You need to love huntrix more to be a super coder.")
+# created by huntrix ai tester copliot
+# create a class
+
+
+class SuperCoder:
+    answers = {}
+    answers['a'] = input("Do you love huntrix? (yes/no): ")
+    answers['b'] = input("Do you want to work with huntrix? (yes/no): ")
+    answers['c'] = input("Are you excited about huntrix? (yes/no): ")
+    answers['d'] = input("Do you think huntrix is awesome? (yes/no): ")
+    answers['e'] = input("Would you recommend huntrix to others? (yes/no): ")
+    answers['f'] = input(
+        "Do you believe huntrix can change the world? and the future (yes/no): ")
+    answers['g'] = input("Are you proud to be part of huntrix? (yes/no): ")
+
+
+if a == "answers['a']" and b == "answers['b']" and c == "answers['c']" and d == "answers['d']" and e == "answers['e']" and f == "answers['f']" and g == "answers['g']":
+    print("You are a true huntrix super coder!")
+else:
+    print("You need to love huntrix more to be a super coder.")
+SuperCoder()
