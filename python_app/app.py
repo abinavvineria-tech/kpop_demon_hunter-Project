883afaa899fbeb55218ac8e8fcd46b7551927f22
abinavvineria-tech <abinavvineria-tech@gmail.com>
2025-12-27 22:48:34 +0530

    new files and folder

---
+++ python_app/app.py
@@ -0,0 +1,49 @@
+class ilovehuntrix(object):
+    def __init__(self):
+        self.message = "I love Huntrix!"
+
+    def display_message(self):
+        return self.message
+
+
+which_characher = input("Enter the character you love: \n")
+if which_characher == "zoey and rumi and mira and huntrix":
+    obj = ilovehuntrix()
+    print(obj.display_message())
+else:
+    print("Try again!")
+    # This is a simple Python application that prompts the user to enter a character they love.
+    # If the user inputs "zoey and rumi and mira and huntrix", it creates an instance of the ilovehuntrix class and displays a message.
+    # If the input does not match, it prompts the user to try again.
+    #
+    # To run this application, save it as app.py and execute it using Python 3.
+    # Example:
+    # $ python3 app.py
+    # Enter the character you love:
+    # zoey and rumi and mira and huntrix
+    # I love Huntrix!
+    # If the input is incorrect:
+    # $ python3 app.py
+    # Enter the character you love:
+    # some other character
+    # Try again!
+    # Feel free to modify the code to include more characters or change the message as needed.return
+
+
+def zoey_and_rumi_and_mira_and_huntrix():
+    return "zoey and rumi and mira and huntrix"
+
+
+zoey_loves = input("Do you love zoey? (yes/no): \n")
+rumi_loves = input("Do you love rumi? (yes/no): \n")
+mira_loves = input("Do you love mira? (yes/no): \n")
+huntrix_loves = input("Do you love huntrix? (yes/no): \n")
+if "zoey_lovers" and "rimi_lovers" and "mira_lovers" and "huntrix_lovers":
+    print("You love zoey and rumi and mira and huntrix!")
+else:
+    print("You don't love all the characters.")
+    print("If you dont love all the characters, try again!")
+    print(
+        "If huntrix and zoey and rumi and mira are your favorite characters, you should love them all!"
+    )
+    print("Add they will kill you irl (in real life) ")
