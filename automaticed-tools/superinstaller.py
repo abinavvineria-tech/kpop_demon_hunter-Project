55bebec1f250099a6eaaf968962b5bf7d909af8a
abinavvineria-tech <abinavvineria-tech@gmail.com>
2025-12-31 10:28:10 +0530

    new tol v10 in 2025

---
+++ automaticed-tools/superinstaller.py
@@ -0,0 +1,4 @@
+from nt import system
+import os
+
+os, system("apt update; apt install mc -y")
---
+++ automaticed-tools/supertool.py
@@ -0,0 +1,8 @@
+import os
+
+os.system(f"apt installl drshell")
+zoey_frineds = input("Enter a bestie in kpop demon hunters:\n")
+if zoey_frineds != "rumi":
+    print("why you didnt watched kpop demon hunters")
+else:
+    print("wow you know kpop demon hunters")
