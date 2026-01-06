088e578bab30fe419c9450470d3fc3ce0c5cdd5e
abinavvineria-tech <abinavvineria-tech@gmail.com>
2026-01-06 20:40:07 +0530

    new code added (super app )

--- huntrix_tools/superapp.py
+++ huntrix_tools/superapp.py
@@ -1,1 +1,11 @@
 import os
+
+os.system("echo ' i love huntrix <3'")
+name = input("Enter a name:\n")
+favchar_in_kpdh = input("Enter your favorite characher in kpop demon hunters:\n")
+print(
+    "your name is: \n",
+    name,
+    "your favorite characher in kpop demon hunters is \n",
+    favchar_in_kpdh,
+)
