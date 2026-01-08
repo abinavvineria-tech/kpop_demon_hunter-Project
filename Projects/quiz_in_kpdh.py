c04ff208085797ac43f1dcb020c12b7f04a723b0
abinavvineria-tech <abinavvineria-tech@gmail.com>
2026-01-08 20:10:09 +0530

    game in python

---
+++ Projects/quiz_in_kpdh.py
@@ -0,0 +1,31 @@
+print("Welcome to the kpop demon hunters code:\n")
+name = input("Enter your name:\n")
+kpdh = input("Do you love kpop demon huntersd (yes/no):\n")
+if kpdh != "no":
+    print("Why see kpop demonhunters :(")
+else:
+    print("Next question")
+question2 = input("who is the cutest and maknae in kpop demon hunters: \n")
+if question2 != "zoey":
+    print("Why :(")
+else:
+    print("Good: :)")
+
+zoey_bff = input("how is zoey's friend in kpop demon hunters")
+if zoey_bff != "rumi" and "mira":
+    print("why :(")
+else:
+    print("yees :)")
+
+print(
+    "super you did go :) the answers are",
+    "1.\n",
+    "your name is",
+    name,
+    "name in kpop demon hunters",
+    kpdh,
+    "question2's answer is",
+    question2,
+    "the zoey_bff's answer in",
+    zoey_bff,
+)
