a955aae6141420df74d53c7b56c9839dc5d36388
abinavvineria-tech <abinavvineria-tech@gmail.com>
2026-01-10 12:49:06 +0530

    modified the code

--- Projects/newcode.py
+++ Projects/newcode.py
@@ -2,12 +2,12 @@ print("Welcome to my new code:\n")
 name = input("ENter you name to continue: \n")
 print
 kpdh = input("Enter your favorite character in kpop demon hunters: \n")
-zoeys_bff = input("How in zoeys bff?: \n")
+zoeys_bff = input("who is zoeys bff?: \n")
 print(
-    "Your name is",
+    "Your name is\n",
     name,
-    "You love: ",
+    "You love: \n",
     kpdh,
-    "zoeys_bff in kpop demon hunters: ",
+    "zoeys_bff in kpop demon hunters: \n",
     zoeys_bff,
 )
