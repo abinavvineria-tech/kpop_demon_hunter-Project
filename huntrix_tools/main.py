24f551387c8b2ff3b1924258f2c4795c72fca586
abinavvineria-tech <abinavvineria-tech@gmail.com>
2026-01-17 17:56:11 +0530

    new files and folders

---
+++ huntrix_tools/main.py
@@ -0,0 +1,11 @@
+print("hi welcome to the main code")
+name = input("Enter a name to continue: \n")
+kpdh_fav_char = input("Enter you favorite characher in kpop demon hunters; \n")
+if name == " ":
+    exit()
+else:
+    print("Yes :) ")
+if kpdh_fav_char == " ":
+    exit()
+else:
+    print("Yes")
